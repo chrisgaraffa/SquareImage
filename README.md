3454821de97eb0125af93bb69d3c2a43efe1c9ec
Chris Garaffa <chris@box8creative.com>
2014-06-24 13:43:45 -0400

    Initial commit
    Haven’t even tested this yet. Just wanted to get something up there.

---
+++ README.md
@@ -0,0 +1,3 @@
+# SquareImage.php
+
+SquareImage.php is a PHP version of some of the code from Reddit's Media.py that finds the most "interesting" parts of an image and cuts off the rest, resulting in a square image. See _image_entropy and _square_image at https://github.com/reddit/reddit/blob/master/r2/r2/lib/media.py.
---
+++ SquareImage.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+* 
+*/
+class SquareImage {
+	var $image;
+	var $thumbnailW = 70;
+	var $thumbnailH = 70;
+	function __construct($path = '') {
+		if (strlen($path)) {
+			$this->setImage($path);
+		}
+	}
+
+	function setImage($path) {
+		$this->image = new Imagick();
+		$this->image->readImageFile($path);
+	}
+
+	function getImage() {
+		return $this->$image;
+	}
+
+	function setThumbnailSize($w, $h) {
+		$this->setThumbnailWidth($w);
+		$this->setThumbnailHeight($h);
+	}
+
+	function setThumbnailWidth($w) {
+		$this->thumbnailW = $w;
+	}
+
+	function setThumbnailHeight($h) {
+		$this->thumbnailH = $h;
+	}
+
+	function getThumbnailWidth() {
+		return $this->thumbnailW;
+	}
+
+	function getThumbnailHeight() {
+		return $this->thumbnailH;
+	}
+
+	function imageEntropy($img) {
+		$hist = $img->getImageHistogram();
+		$hist_size = array_sum($hist);
+		$newHist = array();
+		foreach ($hist as $h) {
+			$newHist[] = float($h) / $hist_size;
+		}
+
+		$returnVal = 0;
+		foreach ($hist as $p) {
+			if ($p > 0) {
+				$returnVal += $p * log($p, 2);
+			}
+		}
+		return -1 * $returnVal;
+	}
+
+	function getSquareImage() {
+		$imageGeometry = $this->image->getImageGeometry();
+		$x = $imageGeometry['width'];
+		$y = $imageGeometry['height'];
+
+		while ($y > $x) {
+			$slice_height = min(y-x, 10);
+
+			$bottom = clone $this->image;
+			$top = clone $this->image;
+
+			$bottom = $bottom->cropImage(0, $y - $slice_height, $x, $y);
+			$top = $top->cropImage(0, 0, $x, $slice_height);
+
+			if ($this->imageEntropy($bottom) < $this->imageEntropy($top)) {
+				$this->image = $this->image->cropImage(0, 0, $x, $y - $slice_height);
+			} else {
+				$this->image = $this->image->crop(0, $slice_height, $x, $y);
+			}
+
+			$imageGeometry = $this->image->getImageGeometry();
+			$x = $imageGeometry['width'];
+			$y = $imageGeometry['height'];
+		}
+
+		return $this->image;
+	}
+}
